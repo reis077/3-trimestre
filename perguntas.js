e4e2fc7e2a8bb9365cc12badcf737205788fb770
reis077 <160619793+reis077@users.noreply.github.com>
2024-10-04 12:21:32 -0300

    ccccc

---
+++ perguntas.js
@@ -0,0 +1,15 @@
+criarCartao(
+    'Geografia',
+    'Qual a capital do Brail?',
+    'Brasília'
+)
+criarCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criarCartao(
+    'Geografia',
+    'Qual a capital dos Estado Unios'
+    'A '
+)
