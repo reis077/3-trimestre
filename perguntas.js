f8802502270458eda5d1f3707b54c4725973e97a
reis077 <160619793+reis077@users.noreply.github.com>
2024-10-18 11:51:53 -0300

    otavio

--- perguntas.js
+++ perguntas.js
@@ -1,15 +1,23 @@
-criarCartao(
-    'Geografia',
-    'Qual a capital do Brail?',
-    'Brasília'
-)
-criarCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
-)
-criarCartao(
-    'Geografia',
-    'Qual a capital dos Estado Unios'
-    'A '
-)
+criaCartao(
+    'Português',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    'Descartes'
+    )
+    
+    criaCartao(
+    'Matemática',
+    'Qual a fórmula para calcular a área de um triângulo?',
+    'c) A = (b x h)/2'
+    )
+    
+    criaCartao(
+    'Hitória',
+    'Onde Alexandre, o Grande foi rei?',
+    'Macedônia'
+    )
+    
+    criaCartao(
+    'Lingua inglesa',
+    'Como se diz como vai em Inglês?',
+    'how are you'
+    )
